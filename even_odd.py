806aa6ccce35500416952d4e7d14ef9853d89507
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2024-05-06 00:12:41 -0700

    Even or odd using function

---
+++ even_odd.py
@@ -0,0 +1,11 @@
+# WAP using function to check whether a given number is even or odd.
+
+def evenOdd():
+    num=int(input("enter the number:"))
+    if (num % 2) == 0:
+        print("the num is even")
+    else:
+        print("the num is odd")
+        return
+evenOdd()
+        
