98e690ea21df02ffe491f44e587105fcf104dcc8
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2024-05-06 23:13:11 -0700

    string concatenation and repeat

---
+++ conca_repeat.py
@@ -0,0 +1,15 @@
+name='mobile'
+bame='phone'
+concatenate=name + bame
+print(concatenate)
+
+concatenate=name + " " + bame
+print(concatenate)
+
+punishment="I won't give Nobita anymore gadgets\n"
+print(punishment*5)
+
+
+
+
+
