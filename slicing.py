82c01702965c37d1f3c237df0bb0b93299302d97
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2024-05-06 23:12:40 -0700

    string slicing

---
+++ slicing.py
@@ -0,0 +1,16 @@
+s='TURA'
+print(s[0:4])
+
+name='Acads'
+print(name[0:3])
+print(name[ :3])
+print(name[3:])
+print(name[:3] + name[3:])
+print(name[0:len(name)])
+print(name[:])
+
+#out of range
+print(name[4:50])
+print(name[40:50])
+print(name[-50:20])
+
