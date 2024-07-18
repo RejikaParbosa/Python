fd4afe9b88f33a8d032f213c22137bb06b3da2f5
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2024-07-18 15:40:18 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,18 @@
+#WAP to perform following tasks:
+#-Read a list of integer from a user
+#-Remove duplicate values from the list. We call it a super list. Display content of the superlist
+
+a=input("Enter the list of integer: ")
+a=list(dict.fromkeys(a)) #remove duplicate
+print(a)
+
+#In a single scan of the super list generated above perform the following operation
+#-If the number are not perfectly divisible by 3, discard it
+#-If the number is greater than 30, add 5 to it. Else subtract 5 from it
+#-Display content of the list
+
+if int(a!%3):
+    
+else:
+    print("a")
+
