75ea9f61f9666c006edf9abb120e271910f401e6
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2024-07-18 15:39:33 +0530

    Add files via upload

---
+++ file_Q3.py
@@ -0,0 +1,10 @@
+#Write a python program to perform writing and reading few lines in a text file
+
+t=open('read_write.txt','w')
+t.write('mango\n')
+t.write('apple\n')
+t.close()
+print(t)
+#n=open('file.txt','r')
+#n.readlines()
+#n.close()
